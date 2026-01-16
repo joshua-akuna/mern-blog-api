58c2e1fe56027760b3b045a2b17e9c42ffb17da0
Akuna Joshua <akunajoshua@gmail.com>
2026-01-16 11:14:20 +0100

    timestamps added to postModel schema

--- models/postModel.js
+++ models/postModel.js
@@ -1,10 +1,15 @@
 const { Schema, model } = require('mongoose');
 
-const PostSchema = new Schema({
-  title: { type: String, required: true },
-  summary: { type: String, required: true },
-  content: { type: String, required: true },
-  cover: { type: String, required: true },
-});
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    summary: { type: String, required: true },
+    content: { type: String, required: true },
+    cover: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model('Post', PostSchema);
