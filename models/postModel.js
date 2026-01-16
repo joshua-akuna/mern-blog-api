744f712791575ed8cc5f2766a8b261dc83ef3bea
Akuna Joshua <akunajoshua@gmail.com>
2026-01-16 09:50:00 +0100

    postModel file created

---
+++ models/postModel.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require('mongoose');
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  summary: { type: String, required: true },
+  content: { type: String, required: true },
+  cover: { type: String, required: true },
+});
+
+module.exports = model('Post', PostSchema);
